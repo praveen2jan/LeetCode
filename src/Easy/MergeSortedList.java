5a13b237155c5fb4631e3e88f056d60caf237808
praveen2jan <praveen1668301@gmail.com>
2022-09-29 10:19:23 +0530

    Merge Sorted LinkedLIst

---
+++ src/Easy/MergeSortedList.java
@@ -0,0 +1,70 @@
+package Easy;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeSortedList {
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        printLL(list1);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        printLL(list2);
+
+        var merged = mergeTwoLists(list1, list2);
+
+        printLL(merged);
+    }
+
+
+    public static void printLL(ListNode list) {
+        while (list!=null) {
+            System.out.print(list.val +" ");
+            list = list.next;
+        }
+        System.out.println();
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(0);
+        ListNode head = res;
+
+        while (list1 != null && list2 != null ){
+            if (list1.val < list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+
+        if(list1 != null) {
+            head.next = list1;
+        }
+        if(list2 != null) {
+            head.next = list2;
+        }
+
+        return res.next;
+    }
+
+}
